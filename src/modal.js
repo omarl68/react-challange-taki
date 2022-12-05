fafa369d659e1ee8aa2f55ae304a9ed6b0e3a23d
Taki Academy <takiacademy@Takis-Mac-mini-5.local>
2022-12-05 17:13:55 +0100

    fix close

--- src/modal.js
+++ src/modal.js
@@ -1,19 +1,22 @@
 const Modal = ({ product, setIsOpen, setDeleteProduct }) => {
   const handleClick = () => {
+
     setDeleteProduct((prev) => {
       prev = prev.filter((item) => item?.id !== product?.id);
+    
       return prev;
     });
     fetch("http://localhost:8000/product/" + product.id, {
       method: "DELETE",
     });
+    
   };
   return (
     <div className="modal-container" id="modal_container">
       <div className="modal">
         <h2>{product.title}</h2>
         <p>{product.description}</p>
-        <button className="delete" onClick={handleClick}>
+        <button className="delete" onClick={(e)=>{handleClick();setIsOpen()}  }>
           Supprimer
         </button>
         <button className="close" onClick={setIsOpen}>
